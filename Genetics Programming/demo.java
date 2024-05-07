b46381ce8195d99f5b3a3d362deec005a7db0baf
Chrinovic Mukanya <164991237+ChrinovicMukanya@users.noreply.github.com>
2024-05-07 19:38:59 +0200

    Add files via upload
    Implementations of DNA and Genetics concepts based on research

---
+++ Genetics Programming/demo.java
@@ -0,0 +1,16 @@
+class Animal{
+    String type;
+    int age;
+
+    public Animal(String type, int age){
+        this.type = type;
+        this.age = age;
+    }
+}
+public class demo
+{
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+    }
+}
