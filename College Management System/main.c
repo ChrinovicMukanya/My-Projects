c01c9d6380a42dcccd3136be56b5b79458dfe06c
Chrinovic Mukanya <chrinovicmu@gmail.com>
2024-06-29 08:01:25 +0200

    Add files via upload
    Updated

--- College Management System/main.c
+++ College Management System/main.c
@@ -3,13 +3,14 @@
 #include <string.h>
 #define IT_COURSE_FEE 72000.00
 #define BUSINESS_COURSE_FEE 65000.00
-#define STR_LEN 25
+#define STR_LEN 250
+
 
 const char * _IT = "IT";
 const char * _Business = "Business";
 
 typedef struct Student{
-	int student_id;
+	long int student_id;
 	char * name;
 	char * surname;
 	char * course;
@@ -18,10 +19,12 @@ typedef struct Student{
 }Student;
 
 typedef Student * student_pointer;
-
 double disply_balance(Student student){
 	return  student.current_fees;
 }
+typedef struct Student_Database{
+		Student s_arr[5];
+} Student_Database;
 
 void update_fees(Student * student,char *course,double amount){
 	student_pointer s_ptr = student;
@@ -38,31 +41,32 @@ void update_fees(Student * student,char *course,double amount){
 		}else
 			s_ptr->outstanding_fees = BUSINESS_COURSE_FEE - amount;
 	}else
-		printf("Invalid course/n");
+		printf("Invalid course\n");
 }
-
-void create_student(Student *new_student,char nme[],char sur[],char crs[], double crf){
+void create_student(Student *new_student,char nme[],char sur[],char crs[] ,double crf){
 	 student_pointer s_ptr = new_student;
 	 s_ptr->name = nme;
 	 s_ptr->surname = sur;
 	 s_ptr->course = crs;
 	 s_ptr->current_fees = crf;
 }
-void search_student(Student s_db[], int len,  char * s_info){
+void search_student(Student s_database[], int len,  char s_info[]){
 		int x;
 		for(x = 0; x < len; ++x){
-			if (strcmp(s_name ,s_db[x].name) == 0 || strcmp(s_info , s_db[x].surname) == 0){
-				printf("Name    :\t%s\n", s_db[x].name);
-				printf("Surname :\t%s\n", s_db[x].surname);
-				printf("Course  :\t%s\n", s_db[x].course);
-				printf("fees paid : %d\n", s_db[x].current_fees);
+			if ((strcmp(s_info ,s_database[x].name)) == 0 || (strcmp(s_info, s_database[x].surname) == 0)){
+				printf("Name    :\t%s\n", s_database[x].name);
+				printf("Surname :\t%s\n", s_database[x].surname);
+				printf("Course  :\t%s\n", s_database[x].course);
+				printf("fees paid : %d\n", s_database[x].current_fees);
 			}
-		}printf("No Student Record\n");
+		}printf("No Student Records");
 }	
 int main(int argc, char * argv[])
 {
-	Student student_db[5];
-	int len = sizeof(student_db) / sizeof(student_db[0]);
+	Student student_database[5];
+        int len = sizeof(student_database) / sizeof(student_database[0]);
+
+//	Student_Database s_dbs = {student_database};
 	
 	int st_num;
 	printf("How Many student do you want to add? \n");
@@ -70,18 +74,17 @@ int main(int argc, char * argv[])
 	
 	int x = 0;
 	while (x < st_num){
-		char name[STR_LEN];
+		char  name[STR_LEN];
 		printf("Insert name : ");
 		fgets(name, STR_LEN, stdin);
-	
+		printf("\n");
 
-		char surname[STR_LEN;
+		char surname[STR_LEN];
 		printf("Insert surname : ");
 		fgets(surname, STR_LEN, stdin);
-	
 
 		char course[STR_LEN];
-		printf("Insert course : ");
+		printf("Insert course (IT/Business) : ");
 		fgets(course, STR_LEN, stdin);
 	
 		double current_fees;
@@ -90,16 +93,25 @@ int main(int argc, char * argv[])
 		
 		double outstanding_fees;
 
-		if (strcmp(course , _IT) == 0){
+		if(strcmp(course, _IT) == 0){
 			outstanding_fees = IT_COURSE_FEE - current_fees;
 		}else if (strcmp(course, _Business) == 0){
+
 			outstanding_fees = BUSINESS_COURSE_FEE - current_fees;
 		}
 
 		Student n_student = {1391374, NULL, NULL, NULL};
 		create_student(&n_student, name, surname , course , current_fees);
-		student_db[x] = n_student;
-		x++;
+	
+		student_database[x] = n_student;
+		++x;
 	}
-}
+	printf("\n");
 
+	char search_name[STR_LEN];
+	printf("Insert name to search :  ");
+	scanf("%s", &search_name);
+
+	search_student(student_database, st_num , search_name);
+}
+	
